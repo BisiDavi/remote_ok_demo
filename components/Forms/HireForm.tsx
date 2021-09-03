b0404c8560cbfe3e88f9c04b4b18655a0e85b368
BisiDavi <oludavidconnect@gmail.com>
2021-09-03 14:46:00 +0100

    tag updated

--- components/Forms/HireForm.tsx
+++ components/Forms/HireForm.tsx
@@ -35,7 +35,7 @@ export default function HireForm() {
           company: "",
           position: "",
           primaryTag: "",
-          tags: [],
+          tags: "",
           location: "",
           showLogo: true,
           sendEmail: true,
