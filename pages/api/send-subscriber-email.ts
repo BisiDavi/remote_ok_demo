592461bd1c592d604aa370f64ed49c0aab5679f5
BisiDavi <oludavidconnect@gmail.com>
2021-09-06 20:30:39 +0100

    api updated

--- pages/api/send-subscriber-email.ts
+++ pages/api/send-subscriber-email.ts
@@ -4,40 +4,48 @@ export default function SendSubscriberEmail(req, res) {
   const { method } = req;
   switch (method) {
     case "POST": {
-      mailjet.connect(
-        "bcb48fa229fa4a413134dab4bc534daf",
-        "319d07f39d9a96e41a5b29cb2e5fcd42"
-      );
-      const request = mailjet.post("send", { version: "v3.1" }).request({
-        Messages: [
-          {
-            From: {
-              Email: "oludavidconnect@gmail.com",
-              Name: "Olubisi",
-            },
-            To: [
-              {
-                Email: req.body.email,
-                Name: req.body.email,
+      try {
+        mailjet.connect(
+          process.env.MAIL_JET_API_PRIMARY,
+          process.env.MAIL_JET_API_SECONDARY
+        );
+        const request = mailjet.post("send", { version: "v3.1" }).request({
+          Messages: [
+            {
+              From: {
+                Email: process.env.HOST_EMAIL,
+                Name: process.env.HOST_USERNAME,
               },
-            ],
-            Subject: `Hello ${req.body.email}`,
-            TextPart: "Thanks for Visiting Remote Demo",
-            HTMLPart: `<div>
+              To: [
+                {
+                  Email: req.body.email,
+                  Name: req.body.email,
+                },
+              ],
+              Subject: `Hello ${req.body.email}`,
+              TextPart: "Thanks for Visiting Remote Demo",
+              HTMLPart: `<div>
             <h3>Dear ${req.body.email}, thanks for subscribing to ${req.body.duration} email on remote demo, thanks for visiting my site.You rock!, Have you tried posting a job? </h3>
             <p>Visit <a href="https://remote-ok-demo.vercel.app/hire-remotely">Remote Demo</a> to post a job.</p>
             </div>`,
-            CustomID: "WelcomeMessage",
-          },
-        ],
-      });
-      request
-        .then((result) => {
-          console.log("result", result.body);
-        })
-        .catch((error) => {
-          console.error("error", error.statusCode);
+              CustomID: "WelcomeMessage",
+            },
+          ],
         });
+        const mailResult = request
+          .then((result) => {
+            console.log("result", result.body);
+          })
+          .catch((error) => {
+            console.error("error", error.statusCode);
+          });
+        return res.status(201).json({
+          success: true,
+          result: mailResult,
+        });
+      } catch (error) {
+        return res.status(400).json({ error, message: "unable to send email" });
+      }
     }
     default:
       return null;
